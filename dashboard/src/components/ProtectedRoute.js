bf905ef23b54bc5ffbba062a6b35385e5e307f6d
S ANIKET <aniketsunkuru@gmail.com>
2026-03-01 18:36:32 +0530

    Comment out redirect logic in ProtectedRoute

--- dashboard/src/components/ProtectedRoute.js
+++ dashboard/src/components/ProtectedRoute.js
@@ -1,11 +1,12 @@
 //it ensures that dashboard can be acessed by loged in,signed up user only
 export default function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
+  const frontendUrl = process.env.REACT_APP_FRONTEND_URL || "http://localhost:3000";
 
-  if (!token) {
-    window.location.href = "http://localhost:3000/login";
-    return null;
-  }
+  // if (!token) {
+  //   window.location.href = `${frontendUrl}/login`;
+  //   return null;
+  // }
 
   return children;
 }
